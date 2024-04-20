496d9dbd07e9f84a22f92e20bb919faf4500b862
Przemek Dudek <przemod77@gmail.com>
2024-04-20 18:36:48 +0200

    spring-userbase working container

---
+++ spring-ub/src/test/java/org/springub/SpringUbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
